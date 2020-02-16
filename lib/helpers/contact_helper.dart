858e748b9c4d6919f6948843ea984fe44d17ca3d
Vitor <vitorbukovitz@hotmail.com>
2020-02-15 21:38:06 -0300

    update contact function

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -72,7 +72,15 @@ class ContactHelper{
     );
   }
 
-  
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(
+      contactTable,
+      contact.toMap(),
+      where: "$idColumn = ?",
+      whereArgs: [contact.id]
+    );
+  }
 
 }
 
