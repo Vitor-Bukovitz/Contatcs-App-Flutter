7b323fb4cbe0b13fb4e335780db3ef4b02a60842
Vitor <vitorbukovitz@hotmail.com>
2020-02-15 22:47:53 -0300

    gesture functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:contacts_app/ui/contact_page.dart';
 import 'package:contacts_app/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ContactPage(),
+    home: HomePage(),
   ));
 }
