7d30057e7d2b2750eab977f5dd67c8ba3c538467
Vitor <vitorbukovitz@hotmail.com>
2020-02-15 22:19:11 -0300

    contact card

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contacts_app/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -40,7 +42,56 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(10),
         itemCount: contacts.length,
         itemBuilder: (context, index){
-          return ;
+          return GestureDetector(
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: contacts[index].img != null ?
+                            FileImage(File(contacts[index].img)) :
+                              AssetImage("images/person.png")
+                        )
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            contacts[index].name ?? "",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Text(
+                            contacts[index].email ?? "",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            contacts[index].phone ?? "",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
         },
       ),
     );
