0a55ee41fb8b6a7dcdfa6634851603831e71237d
Vitor <vitorbukovitz@hotmail.com>
2020-02-15 22:05:55 -0300

    basic layout

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -10,23 +10,39 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
   void initState(){
     super.initState();
-    
-    Contact c = Contact();
-    c.name = "Vitor Bukovitz";
-    c.email = "vitorbukovitz@hotmail.com";
-    c.phone = "47992201728";
-    c.img = "imgtest1";
-
-    helper.saveContact(c);
+    helper.getAllContacts().then((list){
+      setState(() {
+        contacts = list;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contacts"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: contacts.length,
+        itemBuilder: (context, index){
+          return ;
+        },
+      ),
     );
   }
 }
