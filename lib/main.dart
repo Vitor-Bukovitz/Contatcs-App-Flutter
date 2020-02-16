ea0e0e63c7dc5dba227cf6c7d77a803393d4da4b
Vitor <vitorbukovitz@hotmail.com>
2020-02-15 22:27:48 -0300

    contact page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:contacts_app/ui/contact_page.dart';
 import 'package:contacts_app/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: ContactPage(),
     )
   );
 }
